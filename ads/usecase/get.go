b48ce1d979f4bcdd324e651da2c564e86b1e6c74
Anton CAZALET <acazalet@protonmail.com>
2021-10-02 22:52:49 +0200

    fix(ads): fix get all ads call by returning an empty array when no ads are in the database

--- ads/usecase/get.go
+++ ads/usecase/get.go
@@ -12,7 +12,13 @@ func (u UseCase) GetAllAds() GetAllAdsCmd {
 	return func(ctx context.Context) ([]*domain.Ad, error) {
 		u.logger.Info().Msg("Fetching all ads ...")
 		defer u.logger.Info().Msg("All ads fetched !")
-		return u.DB.All()
+		//TODO: In the future, when the Database will be real, find another way to make this code return an empty array
+		all_ads, err := u.DB.All()
+
+		if all_ads == nil {
+			return make([]*domain.Ad, 0), nil
+		}
+		return all_ads, err
 	}
 }
 
@@ -20,7 +26,6 @@ type GetAdByIdCmd func(ctx context.Context, id domain.AdID) (*domain.Ad, error)
 
 func (u UseCase) GetAdById() GetAdByIdCmd {
 	return func(ctx context.Context, id domain.AdID) (*domain.Ad, error) {
-		// TODO: Add auth service check here
 		u.logger.Info().Msgf("Fetching ad by id: %v", id)
 		defer u.logger.Info().Msg("All ads fetched !")
 		return u.DB.ByID(id)

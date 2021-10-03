5606966f0afcf31600eefb0f7fabc91a66ff5257
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-03 18:52:01 +0200

    Add first implementation of database mocking

---
+++ chats/infra/database/memory.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	chats "github.com/There-is-Go-alternative/GoMicroServices/chats/domain"
+)
+
+type MockDatabase struct {
+	Chats []chats.Chat
+	Msgs  []chats.Message
+}
+
+func (d *MockDatabase) CreateChat(chat chats.Chat) (chats.Chat, error) {
+	d.Chats = append(d.Chats, chat)
+	return chat, nil
+}
+
+func (d *MockDatabase) CreateMsg(message chats.Message) (chats.Message, error) {
+	d.Msgs = append(d.Msgs, message)
+	return message, nil
+}
+
+func (d *MockDatabase) GetAllChatsOfUser(user_ID string) ([]chats.Chat, error) {
+	result := []chats.Chat{}
+	for _, current_chat := range d.Chats {
+		for _, elem := range current_chat.UsersIDs {
+			if elem == user_ID {
+				result = append(result, current_chat)
+			}
+		}
+	}
+	return result, nil
+}

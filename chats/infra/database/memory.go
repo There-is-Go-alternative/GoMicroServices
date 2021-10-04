82013cbfc1139dc1be09b831fb944b1c6dbfdd0b
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-04 14:38:57 +0200

    Add functions to manage chats and messages

--- chats/infra/database/memory.go
+++ chats/infra/database/memory.go
@@ -41,3 +41,43 @@ func (d MockDatabase) GetAllMessagesOfChat(chat_ID chats.ChatID) ([]chats.Messag
 	}
 	return result, nil
 }
+
+func (d MockDatabase) GetMessage(message_id string) (chats.Message, error) {
+	for _, current_message := range d.Messages {
+		if current_message.ID == string(message_id) {
+			return current_message
+		}
+	}
+	return nil, nil
+}
+
+func (d MockDatabase) GetChat(chat_id string) (chats.Chat, error) {
+	for _, current_chat := range d.Chats {
+		if current_chat.ID == string(chat_id) {
+			return current_chat
+		}
+	}
+	return nil, nil
+}
+
+func (d *MockDatabase) DeleteChat(chat_id string) bool {
+	for index, current_chat := range d.Chats {
+		if current_chat.ID == string(chat_id) {
+			d.Chats[index] = d.Chats[len(d.Chats)-1]
+			d.Chats = d.Chats[:len(d.Chats)-1]
+			return true
+		}
+	}
+	return false
+}
+
+func (d *MockDatabase) DeleteMessage(message_id string) bool {
+	for index, current_message := range d.Messages {
+		if current_message.ID == string(message_id) {
+			d.Messages[index] = d.Messages[len(d.Messages)-1]
+			d.Messages = d.Messages[:len(d.Messages)-1]
+			return true
+		}
+	}
+	return false
+}

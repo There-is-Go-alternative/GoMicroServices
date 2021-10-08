7c49601ab66ea96b204e919a1de44072f8366fbc
Lina <lina1.kaci@epitech.eu>
2021-10-08 17:51:01 +0200

    update(Errmsg): some error message

--- authentification/database/database.go
+++ authentification/database/database.go
@@ -55,8 +55,8 @@ func (db *MongoDB) FindByEmail(ctx context.Context, email string) (domain.Auth, 
 	var auth domain.Auth
 	err := db.collection.FindOne(ctx, bson.M{"email": email}).Decode(&auth)
 	if err != nil {
-		log.Println(err)
-		return domain.Auth{}, err
+		return domain.Auth{}, fmt.Errorf("Email or password is invalid")
+
 	}
 	return auth, nil
 }
@@ -66,7 +66,8 @@ func (db *MongoDB) FindByID(ctx context.Context, id string) (domain.Auth, error)
 	err := db.collection.FindOne(ctx, bson.M{"id": id}).Decode(&auth)
 	if err != nil {
 		log.Println(err)
-		return domain.Auth{}, err
+		return domain.Auth{}, fmt.Errorf("Email or password is invalid")
+
 	}
 	return auth, nil
 }

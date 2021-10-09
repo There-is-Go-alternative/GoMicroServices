52c3f5947b8daaca9c78de184e39080af08dce81
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-09 17:53:28 +0200

    Add errors handling

---
+++ chats/internal/config/config.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"github.com/There-is-Go-alternative/GoMicroServices/chats/internal/utils"
+)
+
+type Config struct {
+	Host       string `json:"host,omitempty"`
+	Port       string `json:"port,omitempty"`
+	AdEndpoint string `json:"ad_endpoint,omitempty"`
+}
+
+func ParseConfigFromPath(path string) (conf *Config, err error) {
+	err = utils.DecodeJSONFromFile(path, &conf)
+	return
+}

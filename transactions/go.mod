9cabeb0b75ff6a6c1c080229a02f17c9b31e8d3f
Naoufel <nberrada@lithium.paris>
2021-10-12 21:26:09 +0300

    add: database

--- transactions/go.mod
+++ transactions/go.mod
@@ -3,3 +3,9 @@ module github.com/There-is-Go-alternative/GoMicroServices/transactions
 go 1.17
 
 require github.com/pkg/errors v0.9.1
+
+require (
+	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365 // indirect
+	github.com/prisma/prisma-client-go v0.11.0 // indirect
+	github.com/takuoki/gocase v1.0.0 // indirect
+)
--- transactions/main.go
+++ transactions/main.go
@@ -9,6 +9,7 @@ import (
 var (
 	confPath   = flag.String("conf", os.Getenv("CONF_PATH"), "path to the json config file.")
 	fundsToken = flag.String("funds-token", os.Getenv("FUNDS_API_TOKEN"), "token to the funds api")
+	adsToken   = flag.String("ads-token", os.Getenv("ADS_API_TOKEN"), "token to the ads api")
 )
 
 func main() {
@@ -19,6 +20,10 @@ func main() {
 	}
 
 	if *fundsToken == "" {
+		log.Fatal("No way to reach the funds service, no token provided")
+	}
+
+	if *adsToken == "" {
 		log.Fatal("No way to reach the funds service, no token provided")
 	}
 }

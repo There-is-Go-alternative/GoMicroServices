2bae7ca8deabc11e1d627e501d6139be5de6ca6d
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-12 15:09:56 +0200

    Remove useless database mocking

--- chats/main.go
+++ chats/main.go
@@ -14,70 +14,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func Memory() {
-	userA := account.Account{ID: "UserA"}
-	userB := account.Account{ID: "UserB"}
-	userC := account.Account{ID: "UserC"}
-	fmt.Printf("%+v\n", userA.ID)
-	fmt.Printf("%+v\n", userB.ID)
-	fmt.Printf("%+v\n\n", userC.ID)
-
-	chatA := chats.Chat{ID: chats.ChatID("ChatA"), UsersIDs: []account.AccountID{userA.ID, userB.ID}}
-	chatB := chats.Chat{ID: chats.ChatID("ChatB"), UsersIDs: []account.AccountID{userC.ID, userB.ID}}
-	chatC := chats.Chat{ID: chats.ChatID("ChatC"), UsersIDs: []account.AccountID{userA.ID, userC.ID}}
-	fmt.Printf("%+v\n", chatA)
-	fmt.Printf("%+v\n", chatB)
-	fmt.Printf("%+v\n\n", chatC)
-
-	messageA := chats.Message{ID: chats.MessageID("Message A to B"), ChatID: chatA.ID, Content: "hello :)", SenderID: userA.ID.String()}
-	messageB := chats.Message{ID: chats.MessageID("Message C to B"), ChatID: chatB.ID, Content: "hello :)", SenderID: userC.ID.String()}
-	messageC := chats.Message{ID: chats.MessageID("Message A to C"), ChatID: chatC.ID, Content: "hello :)", SenderID: userA.ID.String()}
-	fmt.Printf("%+v\n", messageA)
-	fmt.Printf("%+v\n", messageB)
-	fmt.Printf("%+v\n\n", messageC)
-
-	memory := database.MockDatabase{}
-	memory.CreateChat(chatA)
-	memory.CreateChat(chatB)
-	memory.CreateChat(chatC)
-	memory.CreateMessage(messageA)
-	memory.CreateMessage(messageB)
-	memory.CreateMessage(messageC)
-	fmt.Printf("%+v\n%+v\n\n", memory.Chats, memory.Messages)
-
-	getChat, _ := memory.GetChat("ChatB")
-	fmt.Printf("%+v\n\n", getChat)
-
-	getMsg, err := memory.GetMessage("Message C to B")
-	if err != nil {
-		fmt.Printf("There was an error!")
-	} else {
-		fmt.Printf("%+v\n\n", getMsg)
-	}
-
-	memory.DeleteMessage("Message C to B")
-	getMsg, err = memory.GetMessage("Message C to B")
-	if err != nil {
-		fmt.Printf("There was an error!")
-	} else {
-		fmt.Printf("%+v\n\n", getMsg)
-	}
-
-	getAllChats, err := memory.GetAllChatsOfUser(userA.ID)
-	if err != nil {
-		fmt.Printf("There was an error!")
-	} else {
-		fmt.Printf("%+v\n", getAllChats)
-	}
-
-	getAllMessages, err := memory.GetAllMessagesOfChat(chatB.ID)
-	if err != nil {
-		fmt.Printf("There was an error!")
-	} else {
-		fmt.Printf("%+v\n", getAllMessages)
-	}
-}
-
 var (
 	confPath        = flag.String("conf", os.Getenv("CONF_PATH"), "path to the json config file.")
 	shutdownTimeOut = flag.Int("timeout", 2, "Time out for graceful shutdown, in seconds.")
@@ -115,13 +51,13 @@ func Firebase() {
 	userA := account.Account{ID: "UserA"}
 	userB := account.Account{ID: "UserB"}
 	userC := account.Account{ID: "UserC"}
-	chatA := chats.Chat{ID: chats.ChatID("ChatA"), UsersIDs: []account.AccountID{userA.ID, userB.ID}}
-	chatB := chats.Chat{ID: chats.ChatID("ChatB"), UsersIDs: []account.AccountID{userC.ID, userB.ID}}
-	chatC := chats.Chat{ID: chats.ChatID("ChatC"), UsersIDs: []account.AccountID{userA.ID, userC.ID}}
-	chatD := chats.Chat{ID: chats.ChatID("ChatD"), UsersIDs: []account.AccountID{userA.ID, userB.ID, userC.ID}}
-	messageA := chats.Message{ID: chats.MessageID("Message A to B"), ChatID: chatA.ID, Content: "hello :)", SenderID: userA.ID.String()}
-	messageB := chats.Message{ID: chats.MessageID("Message C to B"), ChatID: chatB.ID, Content: "hello :)", SenderID: userC.ID.String()}
-	messageC := chats.Message{ID: chats.MessageID("Message A to C"), ChatID: chatC.ID, Content: "hello :)", SenderID: userA.ID.String()}
+	chatA := chats.Chat{ID: chats.ChatID("ChatA"), UsersIDs: []string{userA.ID.String(), userB.ID.String()}}
+	chatB := chats.Chat{ID: chats.ChatID("ChatB"), UsersIDs: []string{userC.ID.String(), userB.ID.String()}}
+	chatC := chats.Chat{ID: chats.ChatID("ChatC"), UsersIDs: []string{userA.ID.String(), userC.ID.String()}}
+	chatD := chats.Chat{ID: chats.ChatID("ChatD"), UsersIDs: []string{userA.ID.String(), userB.ID.String(), userC.ID.String()}}
+	messageA := chats.Message{ID: chats.MessageID("Message A to B"), ChatID: chatA.ID.String(), Content: "hello :)", SenderID: userA.ID.String()}
+	messageB := chats.Message{ID: chats.MessageID("Message C to B"), ChatID: chatB.ID.String(), Content: "hello :)", SenderID: userC.ID.String()}
+	messageC := chats.Message{ID: chats.MessageID("Message A to C"), ChatID: chatC.ID.String(), Content: "hello :)", SenderID: userA.ID.String()}
 
 	//Create Chats
 	err = ChatsStorage.CreateChat(ctx, chatA)

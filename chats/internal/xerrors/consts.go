76773c28762be398f5a6be5420f4ebd4c89fdd86
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-12 12:47:55 +0200

    Add http routes

--- chats/internal/xerrors/consts.go
+++ chats/internal/xerrors/consts.go
@@ -1,11 +1,12 @@
 package xerrors
 
 const (
-	InvalidChatID  Error = "Chat ID is invalid"
-	ChatNotFound   Error = "Chat not found"
-	InvalidMessageID  Error = "Message ID is invalid"
-	MessageNotFound   Error = "Message not found"
-	MissingParam Error = "Missing param"
+	InvalidChatID      Error = "Chat ID is invalid"
+	ChatNotFound       Error = "Chat not found"
+	InvalidMessageID   Error = "Message ID is invalid"
+	MessageNotFound    Error = "Message not found"
+	MissingParam       Error = "Missing param"
+	AuthorizationError Error = "Authorization token is not in the headers"
 )
 
 const (

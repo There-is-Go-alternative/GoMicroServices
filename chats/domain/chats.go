991bdffeebf26a18e95b5d6c6877633cf6fc1896
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-14 20:01:55 +0200

    Fix validation of uuids

--- chats/domain/chats.go
+++ chats/domain/chats.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 )
 
@@ -12,9 +14,11 @@ func NewChatID() (ChatID, error) {
 }
 
 // Validate check for ChatID integrity
-func (id ChatID) Validate() bool {
-	// Need to find a way to check if the UUID is valid
-	return true
+func (id ChatID) Validate() error {
+	if _, err := uuid.Parse(id.String()); err != nil {
+		return fmt.Errorf("ID (%v) is invalid: {%v}", id, err)
+	}
+	return nil
 }
 
 // Equal check for ChatID equality
@@ -34,7 +38,7 @@ type Chat struct {
 
 // Validate check presence of minimal data required for an Chat.
 func (c Chat) Validate() bool {
-	return c.ID.Validate()
+	return c.ID.Validate() != nil
 }
 
 func (c Chat) String() string {
--- chats/domain/messages.go
+++ chats/domain/messages.go
@@ -17,9 +17,11 @@ func NewMessageID() (MessageID, error) {
 }
 
 // Validate check for MessageID integrity
-func (id MessageID) Validate() bool {
-	// Need to find a way to check if the UUID is valid
-	return true
+func (id MessageID) Validate() error {
+	if _, err := uuid.Parse(id.String()); err != nil {
+		return fmt.Errorf("ID (%v) is invalid: {%v}", id, err)
+	}
+	return nil
 }
 
 // Equal check for MessageID equality
@@ -43,7 +45,7 @@ type Message struct {
 
 // Validate check presence of minimal data required for an Message.
 func (m Message) Validate() bool {
-	return m.ID.Validate() && m.ChatID != "" && m.Content != "" && m.SenderID != ""
+	return m.ID.Validate() != nil && m.ChatID != "" && m.Content != "" && m.SenderID != ""
 }
 
 func (m Message) String() string {
